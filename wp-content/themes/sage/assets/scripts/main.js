c5f5ee08a5bdd7621f0e581471afa869cc90109e
Michael Church <michael@fishinglounge.com>
2016-05-31 09:53:23 -0400

    updated jumplink js

--- wp-content/themes/sage/assets/scripts/main.js
+++ wp-content/themes/sage/assets/scripts/main.js
@@ -18,45 +18,46 @@
     // All pages
     'common': {
       init: function() {
+        //Smooth Jumplink Scrolling
         var target, scroll;
 
-        $("a[href*=#]:not([href=#])").on("click", function(e) {
-        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-        target = $(this.hash);
-        target = target.length ? target : $("[id=" + this.hash.slice(1) + "]");
+        $("a[href*=\\#]:not([href=\\#])").on("click", function(e) {
+          if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+          target = $(this.hash);
+          target = target.length ? target : $("[id=" + this.hash.slice(1) + "]");
 
-        if (target.length) {
-            if (typeof document.body.style.transitionProperty === 'string') {
-                e.preventDefault();
-              
-                var avail = $(document).height() - $(window).height();
+          if (target.length) {
+          if (typeof document.body.style.transitionProperty === 'string') {
+          e.preventDefault();
 
-                scroll = target.offset().top - 0;
-              
-                if (scroll > avail) {
-                    scroll = avail;
-                }
+          var avail = $(document).height() - $(window).height();
 
-                $("html").css({
-                    "margin-top" : ( $(window).scrollTop() - scroll ) + "px",
-                    "transition" : "1s ease-in-out"
-                }).data("transitioning", true);
-            } else {
-                $("html, body").animate({
-                    scrollTop: scroll
-                }, 1000);
-                return;
+          scroll = target.offset().top - 60;
+
+          if (scroll > avail) {
+          scroll = avail;
+        }
+
+        $("html").css({
+          "margin-top" : ( $(window).scrollTop() - scroll ) + "px",
+          "transition" : "1s ease-in-out"
+          }).data("transitioning", true);
+          } else {
+            $("html, body").animate({
+              scrollTop: scroll
+              }, 1000);
+              return;
+              }
             }
-        }
-        }
+          }
         });
 
         $("html").on("transitionend webkitTransitionEnd msTransitionEnd oTransitionEnd", function (e) {
-        if (e.target === e.currentTarget && $(this).data("transitioning") === true) {
-        $(this).removeAttr("style").data("transitioning", false);
-        $("html, body").scrollTop(scroll);
-        return;
-        }
+          if (e.target === e.currentTarget && $(this).data("transitioning") === true) {
+            $(this).removeAttr("style").data("transitioning", false);
+            $("html, body").scrollTop(scroll);
+            return;
+          }
         });
       },
       finalize: function() {
